0e6b5be028f3c3ec5f6f69d40bda5d561a74242a
abdullahr73 <floyd_1@rogers.com>
2018-04-19 16:12:45 -0400

    Made he MVC to mate the app check mark not disappear when scrolled off the screen. and also cleaned up the code

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,20 +10,29 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Floyd", "Malachi", "Fedora"]
+    var itemArray = [Item]()
     
-    //Create default object to save items in array after app is terminated. (user defaults #1)
+    //Create default object to save items in array after app is terminated. (userDefaults #1)
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //To retrive the data to the tableView (user defaults #3)
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            
-            itemArray = items
-            
-        }
+        //To retrive the data to the tableView (userDefaults #3)
+        
+        let newItem1 = Item()
+        newItem1.title = "Floyd"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Grant"
+        itemArray.append(newItem2)
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//
+//            itemArray = items
+//
+//        }
         
     }
     //Mark - TableView Datasource methods
@@ -36,9 +45,16 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        //Cut down on the the code repeating
+        let item = itemArray[indexPath.row]
         
+        cell.textLabel?.text = item.title
         
+        if item.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         
         return cell
         
@@ -47,17 +63,28 @@ class TodoListViewController: UITableViewController {
     //Mark - TableView Delagate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
+        
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        } else {
+//            itemArray[indexPath.row].done = false
+//        }
+        
+        tableView.reloadData()
         
         //When you click row the highlighted area flashes
         tableView.deselectRow(at: indexPath, animated: true)
         
-        //Selecting and deselecting checkmark for each row
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        //Selecting and deselecting checkmark for each row
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
     }
     
     //Mark - Add New Items
@@ -70,9 +97,12 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What happen onc theuseclic the add  Item buuton on ou UIAlert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            //Set the input item into a localStorage (user defaults #2)
+            self.itemArray.append(newItem)
+            
+            //Set the input item into a localStorage (userDefaults #2)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by floyd Grant on 2018-04-18.
+//  Copyright © 2018 floyd Grant. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
